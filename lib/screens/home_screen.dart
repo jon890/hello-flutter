d4ad07d7566f0e7cdf2c8de6006133574266db1a
BiFoS <jon89071@gmail.com>
2023-06-15 18:46:30 +0900

    :lipstick: Timer 구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  bool isRunning = false;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds--;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(Duration(seconds: 1), onTick);
+
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+
+    setState(() {
+      isRunning = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Container(
                   alignment: Alignment.bottomCenter,
                   child: Text(
-                    '25:00',
+                    "$totalSeconds",
                     style: TextStyle(
                         color: Theme.of(context).cardColor,
                         fontSize: 89,
@@ -31,16 +59,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: IconButton(
                       iconSize: 120,
                       color: Theme.of(context).cardColor,
-                      onPressed: () {},
-                      icon: Icon(Icons.play_circle_outlined)))),
+                      onPressed: isRunning ? onPausePressed : onStartPressed,
+                      icon: Icon(isRunning
+                          ? Icons.pause_circle_outline
+                          : Icons.play_circle_outlined)))),
           Flexible(
               flex: 1,
               child: Row(
                 children: [
                   Expanded(
                     child: Container(
-                        decoration:
-                            BoxDecoration(color: Theme.of(context).cardColor),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).cardColor,
+                            borderRadius: BorderRadius.circular(50)),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
