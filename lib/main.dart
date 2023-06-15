dc5dd9d09f8d8184d6658c074ab3a1405cbc8b3f
BiFoS <jon89071@gmail.com>
2023-06-15 13:41:42 +0900

    :lipstick: Component styling

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
           body: Padding(
               padding: EdgeInsets.symmetric(horizontal: 40),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SizedBox(
                     height: 80,
@@ -34,6 +35,35 @@ class App extends StatelessWidget {
                                   fontSize: 18))
                         ],
                       )
+                    ],
+                  ),
+                  SizedBox(height: 120),
+                  Text("Total Balance",
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.8), fontSize: 22)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("\$5 194 482",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 48,
+                          fontWeight: FontWeight.w600)),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(45)),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 20, horizontal: 50),
+                            child: Text("Transfer",
+                                style: TextStyle(fontSize: 20)),
+                          ))
                     ],
                   )
                 ],
