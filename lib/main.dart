1d217e8aa8aa101b5147038ccbf2c27a76b6113e
BiFoS <jon89071@gmail.com>
2023-06-15 14:26:28 +0900

    :lipstick: My first widget!

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:helloflutter/widgets/button.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
+          backgroundColor: const Color(0xFF181818),
           body: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 80,
                   ),
                   Row(
@@ -24,7 +27,7 @@ class App extends StatelessWidget {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          Text('Hey, Selena',
+                          const Text('Hey, Selena',
                               style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 28,
@@ -37,33 +40,32 @@ class App extends StatelessWidget {
                       )
                     ],
                   ),
-                  SizedBox(height: 120),
+                  const SizedBox(height: 120),
                   Text("Total Balance",
                       style: TextStyle(
                           color: Colors.white.withOpacity(0.8), fontSize: 22)),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
-                  Text("\$5 194 482",
+                  const Text("\$5 194 482",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 48,
                           fontWeight: FontWeight.w600)),
-                  SizedBox(
+                  const SizedBox(
                     height: 30,
                   ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Container(
-                          decoration: BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.circular(45)),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 20, horizontal: 50),
-                            child: Text("Transfer",
-                                style: TextStyle(fontSize: 20)),
-                          ))
+                      Button(
+                          text: "Transfer",
+                          bgColor: Color(0xFFF1B33B),
+                          textColor: Colors.black),
+                      Button(
+                          text: "Request",
+                          bgColor: Color(0xFF1F2123),
+                          textColor: Colors.white)
                     ],
                   )
                 ],
