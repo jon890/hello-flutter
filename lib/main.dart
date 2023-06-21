fdc393257821dff61a35582acc04fdbf9f9f91af
BiFoS <jon89071@gmail.com>
2023-06-21 10:04:37 +0900

    :alien: My first request http api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:helloflutter/screens/webtoon_screen.dart';
+import 'package:helloflutter/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
+
   runApp(const App());
 }
 
---
+++ lib/services/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:helloflutter/models/webtoon_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse("$baseUrl/$today");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
+    }
+
+    throw Error();
+  }
+}
